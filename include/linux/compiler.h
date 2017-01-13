b27009a72bf0565f9066621a7f8a065d466a726a
Kees Cook <keescook@chromium.org>
2017-01-13 11:14:39 -0800

    gcc-plugins: Add structleak for more stack initialization
    This plugin detects any structures that contain __user attributes and
    makes sure it is being fully initialized so that a specific class of
    information exposure is eliminated. (This plugin was originally designed
    to block the exposure of siginfo in CVE-2013-2141.)
    
    Ported from grsecurity/PaX. This version adds a verbose option to the
    plugin and the Kconfig.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -25,8 +25,13 @@
 #endif
 extern void __chk_user_ptr(const volatile void __user *);
 extern void __chk_io_ptr(const volatile void __iomem *);
-#else
-# define __user
+# define ACCESS_PRIVATE(p, member) (*((typeof((p)->member) __force *) &(p)->member))
+#else /* __CHECKER__ */
+# ifdef STRUCTLEAK_PLUGIN
+#  define __user __attribute__((user))
+# else
+#  define __user
+# endif
 # define __kernel
 # define __safe
 # define __force
